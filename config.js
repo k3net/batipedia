6f1250f87fd166f3030784e0eacdf53a8a07698f
root <kornel@k3net.hu>
2019-12-17 09:56:38 +0100

    initial commit

---
+++ config.js
@@ -0,0 +1,54 @@
+var config = {
+ 'language_selector_background_image' : 'img/select-language.jpg',
+ 'hu': {
+   'topics':[
+     {'img':'img/batipedia-temavalaszto-hu-1.jpg'},
+     {'img':'img/batipedia-temavalaszto-hu-2.jpg'},
+     {'img':'img/batipedia-temavalaszto-hu-3.jpg', 'url': 'quiz_2019/index.html?l=hu'}
+   ],
+   'content_1_images':[
+     'img/1-rovar.jpg',
+     'img/2-gyumolcs.jpg',
+     'img/3-nektar.jpg',
+   ],
+   'content_2_images':[
+     'img/4-ver.jpg',
+     'img/5-hus.jpg',
+     'img/6-hal.jpg',
+   ]
+ },
+ 'en': {
+   'topics':[
+     {'img':'img/batipedia-temavalaszto-en-1.jpg'},
+     {'img':'img/batipedia-temavalaszto-en-2.jpg'},
+     {'img':'img/batipedia-temavalaszto-en-3.jpg', 'url': 'quiz_2019/index.html?l=en'}
+   ] ,
+   'content_1_images':[
+     'img/1-rovar.jpg',
+     'img/2-gyumolcs.jpg',
+     'img/3-nektar.jpg',
+   ],
+   'content_2_images':[
+     'img/4-ver.jpg',
+     'img/5-hus.jpg',
+     'img/6-hal.jpg',
+   ]
+ },
+ 'hr': {
+   'topics':[
+     {'img':'img/batipedia-temavalaszto-hr-1.jpg'},
+     {'img':'img/batipedia-temavalaszto-hr-2.jpg'},
+     {'img':'img/batipedia-temavalaszto-hr-3.jpg', 'url': 'quiz_2019/index.html?l=hr'}
+   ] ,
+   'content_1_images':[
+     'img/1-rovar.jpg',
+     'img/2-gyumolcs.jpg',
+     'img/3-nektar.jpg',
+   ],
+   'content_2_images':[
+     'img/4-ver.jpg',
+     'img/5-hus.jpg',
+     'img/6-hal.jpg',
+   ]
+ }
+}

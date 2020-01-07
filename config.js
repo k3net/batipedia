2b1aff81593968034b2972e8c7051970ceb8bab6
K3Net <info@k3net.hu>
2020-01-07 09:26:14 +0100

    conf

--- config.js
+++ config.js
@@ -7,14 +7,29 @@ var config = {
      {'img':'img/batipedia-temavalaszto-hu-3.jpg', 'url': 'quiz_2019/index.html?l=hu'}
    ],
    'content_1_images':[
-     'img/1-rovar.jpg',
-     'img/2-gyumolcs.jpg',
-     'img/3-nektar.jpg',
+'img/h1.jpg',
+'img/h2.jpg',
+'img/h3.jpg',
+'img/h4.jpg',
+'img/h5.jpg',
+'img/h6.jpg',
+'img/h7.jpg',
+'img/h8.jpg',
+'img/h9.jpg',
+'img/h10.jpg',
+'img/h11.jpg',
+'img/h12.jpg',
+'img/h13.jpg',
+'img/h14.jpg',
+'img/h15.jpg',
+'img/h16.jpg',
+	
    ],
    'content_2_images':[
      'img/4-ver.jpg',
      'img/5-hus.jpg',
      'img/6-hal.jpg',
+
    ]
  },
  'en': {
@@ -24,7 +39,7 @@ var config = {
      {'img':'img/batipedia-temavalaszto-en-3.jpg', 'url': 'quiz_2019/index.html?l=en'}
    ] ,
    'content_1_images':[
-     'img/1-rovar.jpg',
+     'img/e1.jpg',
      'img/2-gyumolcs.jpg',
      'img/3-nektar.jpg',
    ],
